c3a3d68141ae6669c17a581b12c1afd4d8edc7a1
zwb <mrs.foo@posteo.de>
2018-10-15 23:27:12 +0200

    Utility-Klasse CounterStopWatch erzeugt - auch als kleine c# Uebrung ;-)

---
+++ consolen_anwendung/DBTest/util/CounterStopWatch.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: user
+ * Date: 15.10.2018
+ * Time: 22:31
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System.Diagnostics;
+
+using System;
+
+namespace DBTest
+{
+	namespace util
+	{
+		/// <summary>
+		/// Eine Stopwatch die auch zählen kann und eine vereinfachte Ausgabemethode hat.
+		/// (zwb: Kleine c# Übung)
+		/// </summary>
+		public class CounterStopWatch : Stopwatch
+		{
+			private int counter = 0;
+			
+			public CounterStopWatch()
+			{
+			}
+			
+			/**
+			 * Reset Methode der Basisklasse überschreiben, um sie durch den Counter zu erweitern.
+			 */
+			public new void Reset()
+			{
+				counter = 0;
+				base.Reset();
+			}
+			
+			public void ResetAndStart()
+			{
+				Reset();
+				Start();
+			}
+		
+			public override string ToString()
+			{
+				if(counter != 0)
+				{
+					return ElapsedMilliseconds.ToString() + " (ms) / " + counter + " operations / " + ElapsedMilliseconds/counter + " (ms/op)";
+				}
+				else 
+				{
+					return ElapsedMilliseconds.ToString() + " (ms) / " + counter + " operations";
+				}
+			}
+			
+			/**
+			 * Definieren des ++-Operators: counter inkrementieren!
+			 */
+			public static CounterStopWatch operator ++(CounterStopWatch w)
+			{
+			    w.counter++;
+			    return w;
+			}
+		}
+	}
+}
