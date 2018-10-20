240900a9411081234c00a54563792c7907e02828
zwb <mrs.foo@posteo.de>
2018-10-20 13:41:28 +0200

    Utils-Klasse als Sammelbecken fuer alle moeglichen Utility-Funktionen angelegt

---
+++ consolen_anwendung/DBTest/util/Utils.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: user
+ * Date: 20.10.2018
+ * Time: 13:29
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace DBTest.util
+{
+	/// <summary>
+	/// Klasse mit allgemeinen Utility-Funktionen, die nirgendwo sonst reinpassen.
+	/// </summary>
+	public static class Utils
+	{
+		//aktuelle Zeit als Integer berechnen
+        public static int current_timestamp()
+        {
+            DateTime date1 = new DateTime(1970, 1, 1);  //Refernzdatum (festgelegt)
+                                                        //DateTime date2 = DateTime.Now;              //jetztiges Datum / Uhrzeit
+            DateTime date2 = DateTime.Now.ToUniversalTime();
+            TimeSpan ts = new TimeSpan(date2.Ticks - date1.Ticks);  // das Delta ermitteln
+                                                                    // Das Delta als gesammtzahl der sekunden ist der Timestamp
+            return (Convert.ToInt32(ts.TotalSeconds));
+        }
+
+        //UnixTimestamp aus der Datenbank in datetime umwandeln
+        public static DateTime TimeStampToDateTime(double unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dtDateTime;
+        }
+	}
+}
