223878d5560c11b1e8cbb3a056422ebdb7839045
zwb <mrs.foo@posteo.de>
2018-10-21 11:24:53 +0200

    UNit-Testprojekt hinzugefuegt

---
+++ consolen_anwendung/DBTestUnitTests/UserControl1.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: user
+ * Date: 21.10.2018
+ * Time: 10:32
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace UnitTests
+{
+	/// <summary>
+	/// Description of UserControl1.
+	/// </summary>
+	public partial class UserControl1 : UserControl
+	{
+		public UserControl1()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
---
+++ consolen_anwendung/DBTestUnitTests/sampleTests/SampleTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace DBTest.test
+{
+	[TestFixture]
+	public class SampleTest
+	{
+		[SetUp]
+		public void setUp()
+		{
+			Console.WriteLine("setUp of SampleTest");
+		}
+		
+		[TearDown]
+		public void tearDown()
+		{
+			Console.WriteLine("tearDown of SampleTest");			
+		}
+		
+		[Test]
+		public void testSuccess()
+		{
+			Console.WriteLine("testSuccess of SampleTest");
+			Assert.IsTrue(true, "This exemplary test is expected to pass");
+		}
+		
+		[Test]
+		public void testFailure()
+		{
+			Console.WriteLine("testFailure of SampleTest");
+			Assert.IsTrue(false, "This exemplary test is expected to fail");
+		}
+	}
+}
